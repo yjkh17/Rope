6d1c39aae2f578af16f65f0817956623b141dfcf
Yousef Jawdat <yj.kh17@gmail.com>
2025-12-27 02:59:08 +0300

    Add overlay window delegate

--- Rope/RopeApp.swift
+++ Rope/RopeApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import AppKit
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    private var overlayWindow: OverlayWindow?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        overlayWindow = OverlayWindow()
+        overlayWindow?.orderFrontRegardless()
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        overlayWindow?.orderOut(nil)
+        overlayWindow = nil
+    }
+}
 
 @main
 struct RopeApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
     }
 }
