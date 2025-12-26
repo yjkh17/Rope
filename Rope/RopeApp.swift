457865ef25f06c9ee889f494a3f4beb5b67a99e1
yjkh17 <yj.kh17@gmail.com>
2025-12-27 02:53:00 +0300

    Initial Commit

---
+++ Rope/RopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RopeApp.swift
+//  Rope
+//
+//  Created by Yousef Jawdat on 27/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct RopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
